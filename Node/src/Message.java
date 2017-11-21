8cfa1cad39027ae0da66a300d197d12e6268c116
Rasika Bandara <rasika777@gmail.com>
2017-11-21 23:57:39 +0530

    Adds initial setup for phase 2

---
+++ Node/src/Message.java
@@ -0,0 +1,22 @@
+//TODO Update class with proper filelds and properties
+class Message
+{
+	private String ip;
+	private int port;	
+
+	public Message()
+	{
+		//this.ip = ip;
+		//this.port = port;
+	}	
+
+	public String getIp()
+	{
+		return this.ip;
+	}
+
+	public int getPort()
+	{
+		return this.port;
+	}
+}
